5e638b522b8704277c2c5540c29c6a8d7297c20b
sergeytura <sergei.pashkov@list.ru>
2023-03-02 14:46:37 +0200

    lint pret husky ok

--- src/Components/Genres/Genres.js
+++ src/Components/Genres/Genres.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './Genres.css'
 
 export default function Genres({ genres, genresDB }) {
+  let genKey = 1
   const elements = []
-  genresDB.map((item) => {
+  genresDB.forEach((item) => {
     genres.forEach((el) => {
       if (el === item.id) elements.push(item.name)
     })
   })
 
-  const movieGenres = elements.map((item, index) => {
-    return <li key={index}>{item}</li>
+  const movieGenres = elements.map((item) => {
+    return <li key={genKey++}>{item}</li>
   })
 
   return <ul className="card__genre">{movieGenres}</ul>
